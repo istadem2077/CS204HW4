2583a7dd276602a1fb8fd2f4a73ff2662a37d849
Ismayil Taghi-Zada <ismayil.taghi-zada@sabanciuniv.edu>
2025-04-04 05:30:22 +0300

    My dumbass suddenly decided to use pointer arithmetic with a linked list for some reason, had to rewrite the implementations. Implemented more operator oveoads. Forgot to update << operator implementation...

--- scatterplot.cpp
+++ scatterplot.cpp
@@ -4,6 +4,18 @@
 
 #include "scatterplot.h"
 
+///* Constructors */
+ScatterPlot::~ScatterPlot() {
+
+}
+
+ScatterPlot::ScatterPlot(const ScatterPlot &other) { // deepcopy constructor
+
+}
+///* End Constructors */
+
+
+///* Member functions */
 int ScatterPlot::getSize() const {
     return size;
 }
@@ -11,14 +23,127 @@ int ScatterPlot::getSize() const {
 Point* ScatterPlot::getHead() const {
     return head;
 }
+
+bool ScatterPlot::duplicate(const ScatterPlot& result, Point *rhs) const {
+    Point* curr = result.head;
+    while (curr) {
+        if (curr->x == rhs->x && curr->y == rhs->y) {
+            return true;
+        }
+        curr = curr->next;
+    }
+    return false;
+}
+
+bool ScatterPlot::equal(const ScatterPlot& result, Point *rhs) const {
+    Point* curr = result.head;
+    while (curr) {
+        if (curr->x != rhs->x && curr->y != rhs->y) {
+            return false;
+        }
+        curr = curr->next;
+    }
+    return true;
+}
+
+ScatterPlot ScatterPlot::operator+(const ScatterPlot &rhs) const {
+    ScatterPlot result(*this);
+    Point* rhs_head = rhs.head;
+    while (rhs_head) {
+        if (!duplicate(result, rhs_head)) {
+            Point* temp = new Point(rhs_head->x, rhs_head->y, result.head);
+            result.head = temp;
+            result.size += 1;
+        }
+        rhs_head = rhs_head->next;
+    }
+    return result;
+}
+
+ScatterPlot ScatterPlot::operator+(Point* rhs) const {
+    ScatterPlot result(*this);
+
+    if (!duplicate(result, rhs)) {
+        Point* temp = new Point((rhs->x), (rhs->y), result.head);
+        result.head = temp;
+        result.size += 1;
+    }
+    return result;
+}
+
+ScatterPlot & ScatterPlot::operator=(const ScatterPlot &rhs) {
+    if (this != &rhs) {
+        Point* current = this->head;
+        while (current) {
+            Point* temp = current;
+            current = current->next;
+            delete temp;
+        }
+
+        this->head = nullptr;
+        this->size = 0;
+
+        Point* temp = rhs.head;
+        while (temp) {
+            Point* newNode = new Point(temp->x, temp->y, this->head);
+            this->head = newNode;
+            temp = temp->next;
+            this->size += 1;
+        }
+    }
+    return *this;
+}
+
+bool ScatterPlot::operator==(const ScatterPlot &rhs) const {
+    if (this->size != rhs.size) {
+        return false;
+    }
+
+    Point* curr = rhs.head;
+    while (curr) {
+        if (!equal(*this, curr)) {
+            return false;
+        }
+        curr = curr->next;
+    }
+    return true;
+}
+
+ScatterPlot & ScatterPlot::operator+=(const ScatterPlot &rhs) {
+    *this = *this + rhs;
+    return *this;
+}
+
+int ScatterPlot::operator[](int index) const {
+
+}
+
+ScatterPlot ScatterPlot::operator*(const ScatterPlot &rhs) const {
+}
+///* End Member functions*/
+
+
+///* Free functions */
+ScatterPlot operator+(const Point &lhs, const Point &rhs) {
+
+}
+
+ScatterPlot operator+(const Point &lhs, const ScatterPlot &rhs) {
+
+}
+
 std::ostream& operator<<(std::ostream& lhs, ScatterPlot &plot) {
     Point* head = plot.getHead();
     int size = plot.getSize();
-    for (int i = 0; i < size; i++) {
-        lhs << "(" << (head+i)->x << "," << (head+i)->y << ")";
-        if (i < size - 1) {
+
+    int count = 0;
+    while (head) {
+        lhs << "(" << (head)->x << "," << (head)->y << ")";
+        if (count < size) {
             lhs << ", ";
         }
+        count++;
+        head = head->next;
     }
     lhs << std::endl;
 
@@ -31,34 +156,6 @@ std::istream& operator>>(std::istream& lhs, Point &point) {
     point = Point(x, y);
     return lhs;
 }
+///* End Free functions */
 
-ScatterPlot ScatterPlot::operator+(const ScatterPlot &rhs) const {
-}
 
-ScatterPlot ScatterPlot::operator+(const Point &rhs) const {
-}
-
-ScatterPlot & ScatterPlot::operator=(const ScatterPlot &rhs) {
-}
-
-bool ScatterPlot::operator==(const ScatterPlot &rhs) const {
-}
-
-ScatterPlot & ScatterPlot::operator+=(const ScatterPlot &rhs) {
-}
-
-int ScatterPlot::operator[](int index) const {
-}
-
-ScatterPlot ScatterPlot::operator*(const ScatterPlot &rhs) const {
-}
-
-ScatterPlot operator+(const Point &lhs, const Point &rhs) {
-    Point lhs1(lhs);
-    return lhs1 += rhs;
-}
-
-ScatterPlot operator+(const Point &lhs, const ScatterPlot &rhs) {
-    Point lhs1(lhs);
-    return lhs1 += rhs;
-}
